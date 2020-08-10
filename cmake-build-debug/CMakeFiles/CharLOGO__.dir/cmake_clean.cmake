24df47be7685feb1f182b918af8529ce857e72b7
Linhk1606 <Linhk1606@outlook.com>
2020-08-10 10:09:43 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/CharLOGO__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CharLOGO__.dir/main.cpp.o"
+  "CharLOGO__.pdb"
+  "CharLOGO__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CharLOGO__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
